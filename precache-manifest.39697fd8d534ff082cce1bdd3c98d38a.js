dd6e54f7defb68a8c637247bfeb9b2fee08cc986
root <root@LAPTOP-NCE7VR0T.localdomain>
2020-05-09 15:06:15 +0200

    Updates

---
+++ precache-manifest.39697fd8d534ff082cce1bdd3c98d38a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b051f505e1b6b4e5713f2e8c657eb5f",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "524366836fd38271b18b",
+    "url": "/react-todo-app/static/css/main.af9716c0.chunk.css"
+  },
+  {
+    "revision": "ab12fdb7ba31af2f2670",
+    "url": "/react-todo-app/static/js/2.7737ff3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.7737ff3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "524366836fd38271b18b",
+    "url": "/react-todo-app/static/js/main.766f9b91.chunk.js"
+  },
+  {
+    "revision": "fb271a8cfa48d47b5554",
+    "url": "/react-todo-app/static/js/runtime-main.1c02f952.js"
+  }
+]);
